3fbebd94a7cc0d5bae03cbfd8dc66802dd5bf74d
Yousef emad mikhail <mad33833@gmail.com>
2024-08-02 16:09:11 +0300

    add finish and show scroe

--- js/quiz.js
+++ js/quiz.js
@@ -8,18 +8,19 @@ export class Quiz{
         document.getElementById("next").addEventListener('click',this.nextQuestion.bind(this))
     }
     displayQuestions(){
+        
         let correctAnswer=this.questions[this.currentQuestion].correct_answer
         let inCorrectAnswers=this.questions[this.currentQuestion].incorrect_answers
         let answers=this.putCorrectAnswerRandomly(correctAnswer,inCorrectAnswers)
-
+        document.getElementById("currentQuestion").innerHTML= this.currentQuestion
+        document.getElementById("totalNumberOfQuestions").innerHTML= this.numOfQuestion
         document.getElementById("question").innerHTML=this.questions[this.currentQuestion].question
         let cartona=``
         answers.forEach(
             function(elem){
                 cartona+=`
                                 <label class="form-check-label">
-                                    <input type="radio" class="form-check-input" name="answer" value="${elem}"
-                                        checked>
+                                    <input type="radio" class="form-check-input" name="answer" value="${elem}">
                                     ${elem}
                                 </label>
                                 <br/>`
@@ -43,7 +44,10 @@ export class Quiz{
             this.displayQuestions()
         }
         else{
-            console.log("finish");
+            $("#quiz").fadeOut(500,()=>{
+                $("#finish").fadeIn(500);
+            });
+            document.getElementById("score").innerHTML=this.score
         }
     }
     checkAnswer(userAnswer,correctAnswer){
