61c656cd60ab1f0e9eea777e48f5289ee80c61ca
Yousef emad mikhail <mad33833@gmail.com>
2024-08-02 15:08:00 +0300

    display quiz

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,28 @@
 export class Quiz{
     constructor(questions){
-        console.log(questions);
+        this.questions=questions
+        this.currentQuestion=0
+        this.displayQuestions()
         
+    }
+    displayQuestions(){
+        let correctAnswer=this.questions[this.currentQuestion].correct_answer
+        let inCorrectAnswers=this.questions[this.currentQuestion].incorrect_answers
+        let answers=[correctAnswer,...inCorrectAnswers]
+        document.getElementById("question").innerHTML=this.questions[this.currentQuestion].question
+        let cartona=``
+        answers.forEach(
+            function(elem){
+                cartona+=`
+                                <label class="form-check-label">
+                                    <input type="radio" class="form-check-input" name="answer" value=""
+                                        checked>
+                                    ${elem}
+                                </label>
+                                <br/>`
+                            }
+                        )
+        document.getElementById("rowAnswer").innerHTML=cartona
     }
 }
 
